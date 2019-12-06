bc82cfdc08046cf36d8d213783b92de067a09c67
our ame <you@example.com>
2019-12-06 19:31:15 +0700

    добавил  заготовку модели,  добавил автогенерацию, добавил  автопути, добавил  миграцию

---
+++ database/factories/RecordFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Record;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Model;
+
+$factory->define(Record::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'desc' => $faker->paragraph(),
+        'content' => $faker->text(),
+        'img_url' => $faker->imageUrl(),
+    ];
+});
---
+++ database/migrations/2019_12_06_115026_create_records_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('desc')->nullable();
+            $table->text('content')->nullable();
+            $table->string('img_url')->nullable();
+            $table->string('repo')->nullable();
+            $table->text('repos')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
