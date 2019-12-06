d36a8c14981a38547833bc140304fd12f8cb749b
our ame <you@example.com>
2019-12-06 19:37:28 +0700

    добавил view для records, добавил маршрут records

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -14,7 +14,7 @@ class RecordController extends Controller
      */
     public function index()
     {
-        //
+        return view('records');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/records', 'RecordController@index')->name('records');
