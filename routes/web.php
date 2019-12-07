878fb9d59c6e8ce8601be773768e5e2ebd48192a
our ame <you@example.com>
2019-12-07 14:36:10 +0700

    поменял начальную страницу

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/', 'RecordController@index')->name('records');
+/*Route::get('/', function () {
     return view('welcome');
-});
+});//*/
 
 Auth::routes();
 
