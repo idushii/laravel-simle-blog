e30aa38966d7971d08e672682317372f8e5beaf2
our ame <you@example.com>
2019-12-07 14:35:54 +0700

    Добавил проверку на авторизацию

--- resources/views/record/view.blade.php
+++ resources/views/record/view.blade.php
@@ -6,15 +6,19 @@
         <div class="col-12 mb-2">
             <div class="card">
                 <div class="card-body">
+                    @auth
                     <a href="{{route('record.edit', ['record' => $record->id])}}" class="btn btn-link btn-sm float-right">Изменить</a>
+                    @endauth
                     <h5 class="card-title">{{$record->title}}</h5>
                     <p class="card-text border-bottom pb-2">{{$record->desc}}</p>
                     <p class="card-text">{{$record->content}}</p>
+                    @auth
                     <form action="{{route('record.edit', ['record' => $record->id])}}" method="POST">
                         @csrf
                         <input type="hidden" name="_method" value="DELETE">
                         <input type="submit" value="Удалить" class="btn btn-outline-danger btn-sm">
                     </form>
+                    @endauth
                 </div>
             </div>
         </div>
